7db1598dd04369dee0b6c49e82d195f6619b5979
Sergiu <asd@asd-PC>
2014-10-05 23:01:57 +0300

    Trying to resolve chained dependencies automatically

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,13 @@
 # Recipe:: default
 #
 # Copyright (c) 2014 The Authors, All Rights Reserved.
+
+include_recipe 'build-essential::default'
+include_recipe 'apt::default'
+include_recipe 'apache2::default'
+include_recipe 'mysql::server'
+include_recipe 'mysql::default'
+include_recipe 'php::default'
+include_recipe 'php::module_mysql'
+include_recipe 'vim::default'
+include_recipe 'sendmail::default'
