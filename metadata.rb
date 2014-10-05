c3ca47243b3e079ea9e1174f722ce508a446c29e
Sergiu <asd@asd-PC>
2014-10-05 23:08:23 +0300

    Some cookbooks may not have default recipes

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'sergiu.ionescu@gmail.com'
 license          'Apache 2.0'
 description      'Installs/Configures lamp'
 long_description 'Installs/Configures lamp with Berkshelf and Vagrant support'
-version          '0.1.1'
+version          '0.1.2'
 
 depends 'apt'
 depends 'mysql'
--- recipes/default.rb
+++ recipes/default.rb
@@ -4,12 +4,12 @@
 #
 # Copyright (c) 2014 The Authors, All Rights Reserved.
 
-include_recipe 'build-essential::default'
-include_recipe 'apt::default'
-include_recipe 'apache2::default'
+include_recipe 'build-essential'
+include_recipe 'apt'
+include_recipe 'apache2'
 include_recipe 'mysql::server'
-include_recipe 'mysql::default'
-include_recipe 'php::default'
+include_recipe 'mysql'
+include_recipe 'php'
 include_recipe 'php::module_mysql'
-include_recipe 'vim::default'
-include_recipe 'sendmail::default'
+include_recipe 'vim'
+include_recipe 'sendmail'
