8e3cae70b1e928517587fa2752c6aef14cc2bebc
Sergiu <asd@asd-PC>
2014-10-23 00:17:06 +0300

    Update cookbook version

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'sergiu.ionescu@gmail.com'
 license          'Apache 2.0'
 description      'Installs/Configures lamp'
 long_description 'Installs/Configures lamp with Berkshelf and Vagrant support'
-version          '0.1.5'
+version          '0.1.6'
 
 depends 'mysql', '= 5.5.3'
 depends 'apt', '~> 2.6.0'
