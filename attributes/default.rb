fd3bed0ad7cf6aeb0da4832870b8fdf0995129d0
Sergiu Ionescu <sergiu.ionescu@gmail.com>
2015-06-20 19:07:44 +0300

    Updated default root password

--- attributes/default.rb
+++ attributes/default.rb
@@ -6,5 +6,5 @@ default['lamp']['share']['group'] = 'root'
 
 default['lamp']['xdebug']['version'] = '2.3.2';
 
-default['lamp']['mysql']['root_password'] = '';
+default['lamp']['mysql']['root_password'] = 'root';
 default['lamp']['mysql']['bind_address'] = '0.0.0.0';
