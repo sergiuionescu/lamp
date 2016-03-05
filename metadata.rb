4ae0029e1ce8ef5126e67610d4439521cb78c853
Sergiu Ionescu <sergiu.ionescu@gmail.com>
2016-03-06 00:38:42 +0200

    Adding source and issues url

--- metadata.rb
+++ metadata.rb
@@ -5,6 +5,8 @@ license          'Apache 2.0'
 description      'Installs/Configures lamp'
 long_description 'Installs/Configures lamp with Berkshelf and Vagrant support'
 version          '0.3.0'
+source_url       'https://github.com/sergiuionescu/lamp' if respond_to?(:source_url)
+issues_url       'https://github.com/sergiuionescu/lamp/issues' if respond_to?(:issues_url)
 
 depends 'mysql'
 depends 'apt'
