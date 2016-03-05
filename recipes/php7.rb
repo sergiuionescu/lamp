632c39af30a174395aa4413179f1419039b57125
Sergiu Ionescu <sergiu.ionescu@gmail.com>
2016-03-06 00:21:58 +0200

    Fixed FC023

--- recipes/php7.rb
+++ recipes/php7.rb
@@ -1,6 +1,5 @@
-if node['php']['version'] >= '7'
-  apt_repository 'ondrej-php' do
-    uri          'ppa:ondrej/php'
-    distribution node['lsb']['codename']
-  end
+apt_repository 'ondrej-php' do
+  uri          'ppa:ondrej/php'
+  distribution node['lsb']['codename']
+  only_if node['php']['version'] >= '7'
 end
